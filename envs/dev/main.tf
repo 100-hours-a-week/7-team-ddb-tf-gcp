b7ec1ff0463ce9a8cb830de200abc306d6d980e0
wcorn <dsk08208@gmail.com>
2025-04-30 14:22:19 +0900

    Feat: terraform backend 작성
    - gcs의 cloud storage를 사용하여 terraform backend 적용
    - prefix는 각환경에 맞는 dev, prod 사용
    - credentials는 secrets하위의 account.json 파일 참조
     #16

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,10 +2,14 @@ terraform {
   required_version = "1.11.4"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.31.1"
     }
-    
+  }
+  backend "gcs" {
+    bucket      = "dolpin-terraform-state-29m1t350"
+    prefix      = "dev"
+    credentials = "../../secrets/account.json"
   }
 }
 
