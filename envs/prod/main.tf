b0484a256955f7ac440d72e47d4c440a4ef160a1
wcorn <dsk08208@gmail.com>
2025-04-30 12:54:48 +0900

    Feat: terraform gcp 설정
    - gcp 6.31.1 버전 사용
    - credentials는 root 폴더에 secret 파일 만들어서 account.json으로 넣어서 사용
    - project id,region은 variables에 하드 코딩
     #15

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,3 +1,16 @@
 terraform {
   required_version = "1.11.4"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.31.1"
+    }
+    
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
 }
