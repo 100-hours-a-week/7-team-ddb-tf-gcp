0140c6f8dddda0e6b3a8a1a9f01a19696b15346e
Dongseok Kang <dsk08208@gmail.com>
2025-05-05 20:09:19 +0900

    Feat: be module 작성 (#39)
    - be module 생성
    - be startup script 작성
      - java 17 버전 설치
      - 추후 be 환경변수 적용
    - be module 작성
    - ssh key는 secret manager에 업로드
    - nat_bastion 인스턴스 태그 기반 라우팅 (22포트)
    - lb port 기반 라우팅
    #30

---
+++ modules/be/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_group" {
+  description = "SelfLink of BE instance group"
+  value       = google_compute_instance_group.be_group.self_link
+}
+
+output "health_check_id" {
+  description = "ID of BE health check"
+  value       = google_compute_health_check.be_hc.id
+}
+
+output "port_name" {
+  description = "Named port key for BE"
+  value       = var.ig_port_name
+}
