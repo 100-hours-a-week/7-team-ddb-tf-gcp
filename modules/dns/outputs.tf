74b2eed00703d72030da834ecc90fb117227cd38
wcorn <dsk08208@gmail.com>
2025-05-08 17:17:17 +0900

    Fix: 도메인별 ssl 인증서 별도 발급
    - 한개의 인증서가 아닌 ssl 인증서 별도로 발급 가능하게 수정
     #24

--- modules/dns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ssl_certificate_id" {
-  description = "SSL certificate의 id"
-  value       = google_compute_managed_ssl_certificate.ssl_cert.id
-}
-
-output "ssl_certificate_self_link" {
-  description = "SSL certificate의 self-link"
-  value       = google_compute_managed_ssl_certificate.ssl_cert.self_link
-}
