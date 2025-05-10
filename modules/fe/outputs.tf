fcf125818504cd2164009f6ee500369f7a3bd22c
wcorn <dsk08208@gmail.com>
2025-05-10 18:57:39 +0900

    Chore: dev 배포
     #24

--- modules/fe/outputs.tf
+++ modules/fe/outputs.tf
@@ -1,14 +1,14 @@
-output "fe_instance_self_link" {
+output "instance_self_link" {
   description = "fe instance의 self_link"
   value       = google_compute_instance.fe.self_link
 }
 
-output "fe_instance_group" {
+output "instance_group" {
   description = "fe instance를 포함한 instance group의 self_link"
   value       = google_compute_instance_group.fe_group.self_link
 }
 
-output "fe_health_check" {
+output "health_check" {
   description = "fe instance group에 연결된 헬스체크 리소스의 ID"
   value       = google_compute_health_check.fe.id
 }
