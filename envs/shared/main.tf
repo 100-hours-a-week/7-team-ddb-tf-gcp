c12e01f2be8483fb929de80a355f5baaffc256ba
lily <gitlab_admin_709c41@example.com>
2025-05-12 14:26:03 +0900

    Feat: shared 환경 구성
     #54

---
+++ envs/shared/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.11.4"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.31.1"
+    }
+  }
+  backend "gcs" {
+    bucket      = "dolpin-terraform-state-29m1t350"
+    prefix     = "shared"
+    credentials = "../../secrets/account.json"
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
