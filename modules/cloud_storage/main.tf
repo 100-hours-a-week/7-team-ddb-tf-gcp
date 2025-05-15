a8f8f148a3d64c0c4b127f103fac4f146417d68d
lily <gitlab_admin_709c41@example.com>
2025-05-15 16:34:47 +0900

    Chore: GCS 버킷을 allUsers에 공개
    - 접근성 확보를 위해 allUsers에게 objectViewer 권한을 부여함
     #60

--- modules/cloud_storage/main.tf
+++ modules/cloud_storage/main.tf
@@ -13,8 +13,6 @@ resource "google_storage_bucket" "image_bucket" {
     max_age_seconds = 3600
   }
 
-  public_access_prevention = "enforced"
-
   versioning {
     enabled = true
   } 
@@ -42,10 +40,9 @@ resource "google_compute_backend_bucket" "image_backend_bucket" {
   enable_cdn  = true 
 } 
 
-data "google_project" "project" {}
-
-resource "google_storage_bucket_iam_member" "cdn_access" {
+# 이미지 공개 읽기 권한 설정
+resource "google_storage_bucket_iam_member" "public_access" { 
   bucket = google_storage_bucket.image_bucket.name
   role   = "roles/storage.objectViewer"
-  member = "serviceAccount:service-${data.google_project.project.number}@gs-project-accounts.iam.gserviceaccount.com"
+  member = "allUsers" 
 }
