181a2c1ec801732bc8d393e9d3f70bda00237b83
Dongseok Kang <dsk08208@gmail.com>
2025-05-15 14:12:29 +0900

    Hotfix: 권한 및 참조 에러 수정 (#68)
    - postgres db user 이름 변경
      - db의 기본 이름과 겹쳐 dolpinuser로 이름 변경
    - cloudsql 모듈 병합
      - cloudsql, cloudsqlnetwork 모듈을 병합
    - cloud sql 백업 버킷 권한 추가
      - cloud sql에 백업 버킷에 삽입, 조회 권한을 추가
    #67

--- modules/cloud_sql/main.tf
+++ modules/cloud_sql/main.tf
@@ -29,3 +29,29 @@ resource "google_sql_user" "default" {
   instance    = google_sql_database_instance.postgres.id
   password_wo = var.db_password
 }
+
+resource "google_storage_bucket_iam_member" "allow_sql_export" {
+  bucket = var.backup_bucket_name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_sql_database_instance.postgres.service_account_email_address}"
+}
+
+resource "google_storage_bucket_iam_member" "allow_sql_import" {
+  bucket = var.backup_bucket_name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_sql_database_instance.postgres.service_account_email_address}"
+}
+
+resource "google_compute_global_address" "private_ip_range" {
+  name          = "cloudsql-peering-range-${var.env}-${var.component}"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = var.prefix_length
+  network       = var.vpc_self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = var.vpc_self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
