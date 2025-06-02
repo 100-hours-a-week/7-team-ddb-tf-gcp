5edf6fe40fa5dda2b327f9fe46b371ce14138adc
wcorn <dsk08208@gmail.com>
2025-06-02 15:04:41 +0900

    Feat: static 모듈에서 사용할 버킷 생성
    - 다른 환경에서 사용할 버킷만을 생성하는 모듈
     #119

---
+++ envs/static/modules/cloud_storage/main.tf
@@ -0,0 +1,20 @@
+# 이미지 저장용 GCS 버킷 생성
+resource "google_storage_bucket" "bucket" {
+  name     = var.bucket_name
+  location = var.bucket_location
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+
+  labels = {
+    name        = "${var.env}-bucket"
+    environment = var.env
+    component   = "static"
+    type        = "gcs"
+    managed_by  = "terraform"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
