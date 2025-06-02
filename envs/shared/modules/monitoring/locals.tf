b590f6336e1059ed9694192f20148c82c80cd237
Dongseok Kang <dsk08208@gmail.com>
2025-06-02 23:36:00 +0900

    Feat: 모니터링 데이터 백업  (#118)
    - init 초기에 모든 환경에서 실행으로 수정
    - terraform 자동화 스크립트 shared 환경 dev 브랜치로 변경
    - monitoring instance 권한 추가
    - secret_accessor, storage_admin, compute_viewer, sotrage_object_admin 권한 추가
    - scopes 전체 활성화
     -thanos 추가
    - 30분에 한번씩 prometheus 데이터 업로드
    - 5분에 한번씩 loki 데이터 업로드
    - thanos 0.38.0 버전 사용
    - thanos sidecar, query, store 전부 docker compose로 관리
    - host에 바인드된 볼륨 docker managed volume으로 변경
    - stratup.sh에 반영
     #117

--- envs/shared/modules/monitoring/locals.tf
+++ envs/shared/modules/monitoring/locals.tf
@@ -3,13 +3,18 @@ locals {
   dockercompose_content = file("${path.module}/files/docker-compose.yml")
   prometheus_content    = file("${path.module}/files/prometheus.yml")
   loki_content          = file("${path.module}/files/loki.yml")
+  thanosgcs_content     = file("${path.module}/files/thanos-gcs.yml")
+  endpoints_content     = file("${path.module}/files/endpoints.yml")
 
   rendered_startup_script = templatefile("${path.module}/scripts/startup.sh", {
     name                  = "monitoring"
     dockercompose_content = local.dockercompose_content
     prometheus_content    = local.prometheus_content
     loki_content          = local.loki_content
+    thanosgcs_content     = local.thanosgcs_content
+    endpoints_content     = local.endpoints_content
   })
+  
   ssh_key_entries = [
     for user in var.ssh_users :
     "${user}:${var.public_key}"
