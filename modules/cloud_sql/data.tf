66269b890254a37e6b5b3a7b993cc8117197f015
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-09 17:20:33 +0900

    Feat: cloud sql module 수정-gke 환경
    - secret manager에 public ip 저장하는 코드 삭제
    - random password 생성 후 password로 지정
     #133

--- modules/cloud_sql/data.tf
+++
@@ -1,4 +0,0 @@
-data "google_secret_manager_secret_version" "db_password" {
-  secret = "projects/${var.project_id}/secrets/cloudsql-dolpinuser-password-${var.env}"
-  version = "latest"
-}
--- modules/cloud_sql/variables.tf
+++ modules/cloud_sql/variables.tf
@@ -18,11 +18,6 @@ variable "tier" {
   }
 }
 
-variable "vpc_network_id" {
-  description = "Cloud SQL에 private ip로 연결하기 위한 vpc network resource id"
-  type        = string
-}
-
 variable "resource_type" {
   description = "resource type, 필터링 용도"
   type        = string
@@ -45,22 +40,7 @@ variable "db_name" {
   type        = string
 }
 
-variable "backup_bucket_name" {
-  description = "백업할 bucket 이름"
-  type = string
-}
-
 variable "nat_ip_address" {
   description = "nat의 ip 주소"
-  type = string
-}
-
-variable "project_id" {
-  description = "프로젝트 id"
-  type = string
-}
-
-variable "shared_nat_ip_address" {
-  description = "공유 vpc 의 nat ip 주소"
   type = string
 }
